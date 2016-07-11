4f47831793c79c11491c7b435419bb67f299bbcb
taolichao <xvhandong@sina.com>
2016-07-11 17:53:45 +0800

    去掉没有用的导入包

--- src/com/server/action/CityAction.java
+++ src/com/server/action/CityAction.java
@@ -5,18 +5,15 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.server.dao.CityDao;
-import com.server.pojo.City;
-import com.server.poco.AddressPoco;
 import com.server.poco.CityPoco;
+import com.server.pojo.City;
 import com.system.tools.CommonConst;
-import com.system.tools.base.BaseAction;
 import com.system.tools.base.BaseActionDao;
 import com.system.tools.pojo.Fileinfo;
+import com.system.tools.pojo.Pageinfo;
 import com.system.tools.pojo.Queryinfo;
 import com.system.tools.util.CommonUtil;
 import com.system.tools.util.FileUtil;
-import com.system.tools.pojo.Pageinfo;
 
 /**
  * 城市和县及街道 逻辑层
